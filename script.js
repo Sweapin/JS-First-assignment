453a7f781c5b50b4b2592eb5d114411f3f030a32
Sweapin <lillealex4@hotmail.se>
2021-12-13 18:06:24 +0100

    git init

---
+++ script.js
@@ -0,0 +1,52 @@
+
+//Calculating the are of the triangles
+
+// First Case
+
+const triangleOneBase = 10;
+const triangleOneHeight = 13;
+const triangleOneArea = ( (triangleOneBase * triangleOneHeight) / 2)
+console.log(triangleOneArea);
+
+const triangleTwoBase = 16.5;
+const triangleTwoHeight = 20.3;
+const triangleTwoArea = ( (triangleTwoBase * triangleTwoHeight) / 2);
+console.log(triangleTwoArea);
+
+// Which means Triangle one has an area of 65 meanwhile triangle two
+//has an area of 167.475 so triangle two is way bigger.
+
+// Second case
+
+const triangleThreeBase = 16.5;
+const triangleThreeHeight = 20.3;
+const triangleThreeArea = ( (triangleThreeBase * triangleThreeHeight) / 2);
+console.log(triangleThreeArea);
+
+const triangleFourBase = 20.3;
+const triangleFourHeight = 16.5;
+const triangleFourArea = ( (triangleFourBase * triangleFourHeight) / 2);
+console.log(triangleFourArea);
+
+// Triangle Three & Four has the exact same area
+
+const triangleFiveBase = 7.8;
+const triangleFiveHeight = 5.6;
+const triangleFiveArea = ( (triangleFiveBase * triangleFiveHeight) / 2);
+console.log(triangleFiveArea);
+
+const triangleSixBase = 9.3;
+const triangleSixHeight = 12.4;
+const triangleSixArea = ( (triangleSixBase * triangleSixHeight) / 2);
+console.log(triangleSixArea);
+
+
+// So Triangle five has an area of 21.84
+// Triangle six has an area of ~ 57.66 which means triangle six is bigger.
+
+
+
+// The question:
+
+// We use parentheses to let JS know which order we want things to be
+//calculated. If we don't specify with parentheses it will calculate it wrong.
